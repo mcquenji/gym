da5eec288e36e61691edc61e73403a31698ac854
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2024-02-23 21:32:58 +0100

    Update login screen navigation

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -47,7 +47,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     var user = ref.watch(userProvider);
 
     if (user != null) {
-      context.pushRoute(const OnboardingRoute());
+      context.pushRoute(const HomeRoute());
     }
 
     return Scaffold(
